5d93d6149b363b9b9202affe8bc430304537a865
GiovannaBrizola <133990989+GiovannaBrizola@users.noreply.github.com>
2024-02-29 14:17:40 -0300

    Add files via upload

---
+++ exemploDTO/src/main/java/com/example/DTO/LivroDTOAplication.java
@@ -0,0 +1,12 @@
+package com.example.DTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroDTOAplication{
+	public static void main(String[] args) {
+		SpringApplication.run(LivroDTOAplication.class, args);
+	} 
+
+}
---
+++ exemploDTO/src/main/java/com/exampleDTO/repository/LivroRepository.java
@@ -0,0 +1,8 @@
+package com.exampleDTO.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.exampleDTO.entities.Livro;
+
+public interface LivroRepository extends JpaRepository<Livro,Long> {
+}
